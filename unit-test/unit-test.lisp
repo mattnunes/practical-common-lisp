aa730e7808d825d1658c25573f468c867150fefd
Matt Nunes <matt@mattnunes.com>
2014-08-23 22:59:43 -0700

    Finishing up the unit test framework with test names and docstrings.
    Signed-off-by: Matt Nunes <matt@mattnunes.com>

--- unit-test/unit-test.lisp
+++ unit-test/unit-test.lisp
@@ -2,16 +2,18 @@
 ;;;; From Practical Common Lisp, Chapter 9
 ;;;; Matthew Nunes
 
-(defun report-result (result form)
-  (format t "~:[FAIL~;pass~] ... ~a~%" result form)
-  result)
+(defvar *unit-test-name* nil)
 
 (defmacro with-gensyms ((&rest variables) &body body)
+  "Macro which (gensym)s a list a variables for use within 
+  a macro expansion's body."
   `(let
        ,(loop for variable in variables collecting `(,variable (gensym)))
      ,@body))
 
 (defmacro combine-results (&rest results)
+  "Works like (AND ...) but will evaluate every single 
+  form in results, rather than short-circuiting."
   (with-gensyms (result-p)
     `(progn 
        (let ((,result-p t))
@@ -19,5 +21,20 @@
 	 ,result-p))))
 
 (defmacro check (&rest forms)
+  "Reports the (combine-results ...) of running 
+  (report-result ...) on each form in forms."
   `(combine-results
        ,@(loop for form in forms collecting `(report-result ,form ',form))))
+
+(defmacro deftest (name parameters &body body)
+  "Defines a unit test, from within which you can call 
+  other tests defined with (deftest ...) or call (check ...)"
+  `(defun ,name ,parameters
+     (let ((*unit-test-name* (append *unit-test-name* (list ',name))))
+       ,@body)))
+
+(defun report-result (result form)
+  "Prints FAIL or pass, the originating test function hierarcy,
+  and the tested form, and returns the evaluated result, T or NIL."
+  (format t "~:[FAIL~;pass~] ... ~a: ~a~%" result *unit-test-name* form)
+  result)
